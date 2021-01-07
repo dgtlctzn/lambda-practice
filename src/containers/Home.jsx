38b74f934da97865be425270b30eaafdfa7a20b5
Joseph Perry <josephperry720@gmail.com>
2021-01-07 15:30:21 -0500

    show joke button

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,10 +4,14 @@ import axios from "axios";
 const Home = () => {
 
     const [joke, setJoke] = useState("");
+    const [punchline, setPunchline] = useState("");
+    const [showPunchline, setShowPunchline] = useState(false)
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("click")
+        setShowPunchline(false)
 
         axios({
             method: "GET",
@@ -15,10 +19,16 @@ const Home = () => {
         }).then(res => {
             console.log(res);
             const {setup, punchline} = res.data.body[0];
-            setJoke(setup + "\n" + punchline);
+            setJoke(setup);
+            setPunchline(punchline);
+            setShowPunchline(true);
         }).catch(err => {
             console.log(err);
         })
+    }
+
+    const handleTellMe = () => {
+        setShowPunchline(true);
     }
 
     return (
@@ -26,6 +36,8 @@ const Home = () => {
             <h1>Want to hear a joke?</h1>
             <button onClick={(e) => handleSubmit(e)}>Yes</button>
             <p>{joke}</p>
+            {joke.length ? <button onClick={handleTellMe}>Tell me!</button> : <></>}
+            {showPunchline ? <p>{punchline}</p> : <></>}
         </div>
     );
 };
