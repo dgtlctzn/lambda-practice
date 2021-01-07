c92a466b348432a644004b79077466b8f8c3b260
Joseph Perry <josephperry720@gmail.com>
2021-01-07 14:48:38 -0500

    configured lambda API call

---
+++ functions/API.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+var options = {
+  method: "GET",
+  url: "https://dad-jokes.p.rapidapi.com/random/joke",
+  headers: {
+    "x-rapidapi-key": "59d0c27c79msh6e6814003e3803ep1e5484jsn5fecf295231f",
+    "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
+  },
+};
+
+exports.handler = async (event, context) => {
+    const {data} = await axios(options);
+    return {
+        status: 200,
+        body: data
+    }
+};
