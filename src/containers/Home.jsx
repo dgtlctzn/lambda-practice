7bf6ef3552039fd6d29b14eee85d479d47663ed1
Joseph Perry <josephperry720@gmail.com>
2021-01-07 14:32:18 -0500

    first commit

---
+++ src/containers/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Try some lambda!</h1>
+            <button>Press</button>
+        </div>
+    );
+};
+
+export default Home;
